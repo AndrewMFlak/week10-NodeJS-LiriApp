ca850e7784cef27354d634b693e6f13014fa047e
AFlak <AndrewMFlak@gmail.com>
2018-04-27 00:28:56 -0400

    updated data in logic

--- logic.js
+++ logic.js
@@ -1,10 +1,12 @@
 var Twitter = require('twitter');
+
+var omdbApi = require('omdb-client');
  
 var client = new Twitter({
-  consumer_key: '',
-  consumer_secret: '',
-  access_token_key: '',
-  access_token_secret: ''
+  consumer_key: 'process.env.TWITTER_CONSUMER_KEY',
+  consumer_secret:'process.env.TWITTER_CONSUMER_SECRET',
+  access_token_key: 'process.env.TWITTER_ACCESS_TOKEN_KEY',
+  access_token_secret: 'process.env.TWITTER_ACCESS_TOKEN_SECRET'
 });
  
 var params = {screen_name: 'nodejs'};
@@ -12,4 +14,14 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
     console.log(tweets);
   }
+});
+
+ 
+var params = {
+    apiKey: 'XXXXXXX',
+    query: 'Terminator',
+    year: 2012
+}
+omdbApi.search(params, function(err, data) {
+    // process response...
 });
