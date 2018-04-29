4c3df18bf80948653a5314b2d979ae63cb4c663c
AFlak <AndrewMFlak@gmail.com>
2018-04-29 18:12:16 -0400

    updated content with functioning omdb

---
+++ keys.js
@@ -0,0 +1,17 @@
+console.log('this is loaded');
+
+exports.twitter = {
+
+    consumer_key: 'QKXiOVxdZGNEQDZYbRTAE49Jp',
+
+    consumer_secret: 'Y1az5fHYY94QK8U9yIyVpYFaxOXKWdN7mK0HZxtgq5fHz2HJSK',
+
+    access_token_key: '988554447273029632-Nt1Pj9SFZ07yGyIBD2mN5sUY1qvGTE4',
+
+    access_token_key: 'y3rT2cLNNQxfGBMKX5WvHaU1lCtw59DUtVk5cgDhLJXmr'
+};
+
+exports.spotify = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+};
--- logic.js
+++ logic.js
@@ -1,7 +1,11 @@
 var Twitter = require('twitter');
 
 var omdbApi = require('omdb-client');
- 
+
+var spotify = require('spotify');
+
+
+//  twitter api key storage in .env file
 var client = new Twitter({
   consumer_key: 'process.env.TWITTER_CONSUMER_KEY',
   consumer_secret:'process.env.TWITTER_CONSUMER_SECRET',
@@ -16,7 +20,7 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
   }
 });
 
- 
+//  omdb client access data
 var params = {
     apiKey: 'XXXXXXX',
     query: 'Terminator',
@@ -24,4 +28,14 @@ var params = {
 }
 omdbApi.search(params, function(err, data) {
     // process response...
+});
+
+//  spotify access data
+spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+ 
+    // Do something with 'data' 
 });
